4f1be01788775bd12abc8925d7405c76d53fdeeb
Hilal DAVAS <95532835+hilalldavas@users.noreply.github.com>
2024-05-23 22:17:20 +0300

    sözlük ekranı eklendi stil dosyası düzenlendi

--- scripts/dayScript.js
+++ scripts/dayScript.js
@@ -195,6 +195,7 @@ function openNav() {
     document.getElementById("drawerSpan").style.display = "none";
     document.getElementById("pauseButton").style.visibility = "hidden";
     document.getElementById("attackListContainer").style.visibility = "hidden";
+    document.getElementById("graf-dropdown").style.visibility="hidden";
 }
 
 function closeNav() {
@@ -202,6 +203,7 @@ function closeNav() {
     document.getElementById("drawerSpan").style.display = "block";
     document.getElementById("pauseButton").style.visibility = "visible";
     document.getElementById("attackListContainer").style.visibility = "visible";
+    document.getElementById("graf-dropdown").style.visibility="visible";
 }
 
 function pause() {
